d86f7a741742b069016b790fa5b6afe17a3d5da7
Yago Lopes <yagolopeslazaro@gmail.com>
2019-05-24 14:32:48 -0300

    função para trazer mensagens externas

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,3 +1,6 @@
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
@@ -6,8 +9,22 @@ const httpLink = new HttpLink({
   uri: "https://api.graph.cool/simple/v1/cjw0s740h2x610128bpihc9v9"
 });
 
+const wsLink = new WebSocketLink({
+  uri: "wss://subscriptions.us-west-2.graph.cool/v1/cjw0s740h2x610128bpihc9v9",
+  options: { reconnect: true }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache()
 });
 
--- src/pages/chat/index.js
+++ src/pages/chat/index.js
@@ -16,6 +16,34 @@ import Input from "../input";
 const author = "Yago";
 
 class Chat extends Component {
+  componentDidMount() {
+    this.props.conversation.subscribeToMore({
+      document: gql`
+        subscription onMessageAdded($author: String!) {
+          Message(
+            filter: { mutation_in: [CREATED], node: { from_not: $author } }
+          ) {
+            node {
+              id
+              from
+              message
+            }
+          }
+        }
+      `,
+      variables: {
+        author
+      },
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data["Message"]) return prev;
+
+        const newItem = subscriptionData.data["Message"].node;
+
+        return { ...prev, allMessages: [...prev.allMessages, newItem] };
+      }
+    });
+  }
+
   componentDidUpdate() {
     setTimeout(() => {
       this.scrollView.scrollToEnd({ animated: false });
